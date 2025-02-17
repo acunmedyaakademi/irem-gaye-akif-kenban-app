41ec75261a4cf37863cfa7687970411724478fb3
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2025-02-17 11:27:28 +0300

    open board sayfası yapımına baslandi

--- src/components/NewBoard.jsx
+++ src/components/NewBoard.jsx
@@ -1,7 +1,30 @@
+import "/style/new-board.css";
+
 export default function NewBoard() {
   return(
     <>
-    new sayfası
+    <div className="new-board-container">
+      <h2>Add New Board</h2>
+      <form>
+        <div className="board-name-area">
+          <label htmlFor="name">Board Name</label>
+          <div>
+          <input type="text" name="name" placeholder="e.g. Web Design"/>
+          </div>
+        </div>
+        <div className="column-area">
+          <label htmlFor="columns">Board Columns</label>
+          <div className="flex">
+            <input type="text" name="columns"/>
+            <img src="/assets/images/cancel-icon.svg"/>
+          </div>
+        </div>
+        <div className="button-area">
+        <button>+ Add New Column</button>
+        <button>Create New Board</button>
+        </div>
+      </form>
+    </div>
     </>
   )
 }
