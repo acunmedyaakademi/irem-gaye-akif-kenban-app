b661edcac36a46f1d61146fffdfa177293061a83
Mehmet Akif  Kücükyilmazz <makif4307@hotmail.com>
2025-02-17 10:00:35 +0300

    baslangic düzenlemeleri yapıildi

---
+++ helper.jsx
@@ -0,0 +1,36 @@
+import Suggestions from "./components/Suggestions";
+import Roadmap from "./components/Roadmap";
+import FeedbackDetail from "./components/FeedbackDetail";
+import  NewFeedback  from "./components/NewFeedback";
+
+
+const routes = [
+  { title: "Home", url: "/", component: <Suggestions /> },
+  { title: "Roadmap", url: "/roadmap", component: <Roadmap /> },
+  { url: "/feedback-detail", component: <FeedbackDetail /> },
+  { url: "/roadmap/", component: <Roadmap /> },   
+  { title: 'New Feedback', url: '/new-feedback', component: <NewFeedback /> },
+];
+
+const notFound = {
+  component: <NotFound />,
+};
+
+export function getPage(url) {
+  const exactPage = routes.find((x) => x.url === url);
+  if (exactPage) return exactPage;
+
+  if (url.startsWith("/feedback-detail/")) {
+    return { title: "Feedback Detail", component: <FeedbackDetail /> };
+  }
+
+  if (url.startsWith("/roadmap/")) {
+    return { title: "Roadmap Filtered", component: <Roadmap /> };
+  }
+
+  return notFound;
+}
+
+function NotFound() {
+  return <h1>Bu sayfa bulunamadı.</h1>;
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import "/style/reset.css"
+
 
 function App() {
+
+  const [data,setData] = useState([])
+
+  useEffect(() => {
+    async function getData() {
+      const data = await fetch("data/data.json").then((r) => r.json());
+      setData(data);
+      console.log(data);
+    }
+    getData();
+}, []);
 
   return (
     <>
