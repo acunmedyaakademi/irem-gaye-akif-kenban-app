3fd6bb8b06cccc74c17506c1f79e37fd6d14db1f
[Gaye Dinç] <gayedinc190@gmail.com>
2025-02-17 09:40:34 +0300

    birkaç düzenleme

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-   
+      <h1>gaye</h1>
     </>
   )
 }
 
-export default App
+export default App;
