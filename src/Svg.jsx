92f25bd1cb41e5421862806b47794395b41949e4
[Gaye Dinç] <gayedinc190@gmail.com>
2025-02-17 11:26:41 +0300

    header sayfası oluşturuluyor

---
+++ src/Svg.jsx
@@ -0,0 +1,13 @@
+export function DownSvg() {
+  return (
+    <svg
+      width="10"
+      height="5"
+      viewBox="0 0 10 5"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M1 0L5 4L9 0" stroke="#635FC7" strokeWidth="2" />
+    </svg>
+  );
+}
